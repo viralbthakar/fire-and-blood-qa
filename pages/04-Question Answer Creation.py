9df05576467457aa634825532a473005fb2e7a7a
viralbthakar <viralbthakar@gmail.com>
2023-04-05 10:33:04 -0400

    QA Model

--- pages/04-Question Answer Creation.py
+++ pages/04-Question Answer Creation.py
@@ -65,10 +65,10 @@ if debug_qa_extraction:
                     for index, row in data_df.iterrows():
                         paragraph = row['cleaned_paragraphs']
                         sentences = sent_tokenize(paragraph)
-                        for sentence in sentences:
+                        for sentence in sentences[1000]:
                             doc = extract_NER(sentence, model="en_core_web_lg")
                             qas = create_qa_pairs_from_NER(doc)
-                            for que, ans in qas:
+                            for que, ans in qas[0]:
                                 question_answers["context"].append(sentence)
                                 question_answers["questions"].append(que)
                                 question_answers["answers"].append(ans)
--- pages/05-Question Answer Model.py
+++ pages/05-Question Answer Model.py
@@ -6,7 +6,7 @@ from collections import defaultdict
 from nltk.tokenize import word_tokenize, sent_tokenize
 from streamlit_extras.switch_page_button import switch_page
 from src.utils import styled_print, create_dir, fetch_session_state, add_to_session_state, extract_NER, create_qa_pairs_from_NER
-
+from transformers import pipeline
 # Set the title and instructions for the page
 styled_print("Initiating Question Answer Model", header=True)
 
@@ -24,3 +24,17 @@ st.set_page_config(
 
 st.title("Question Answer Model Creation Tool")
 st.write("Tool to create question, answer model")
+
+
+genre = st.radio(
+    "Select Method to Use",
+    ('Pretrained BERT', 'Other'))
+
+question_txt = st.text_area('Question')
+context_txt = st.text_area('Context')
+qa_button = st.button("Send A Raven!")
+
+if qa_button:
+    question_answerer = pipeline(
+        "question-answering", model="distilbert-base-cased-distilled-squad")
+    st.write(question_answerer(question=question_txt, context=context_txt))
